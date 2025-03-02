ba47824d76b15c68a01333865dd643f5614b85a6
Enzo Vigneti <enzo@amalgama.co>
2025-03-01 21:37:18 -0300

    Add status endpoint

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import psutil
+import subprocess
+import os
 
 app = FastAPI()
 
@@ -8,6 +11,31 @@ def read_root():
     return {"message": "Hello FastAPI"}
 
 
-@app.get("/test")
-def read_test():
-    return {"message": "Hello Raspberry Pi"}
+@app.get("/logs")
+def read_logs():
+    with open("logs.txt", "r") as file:
+        return file.read()
+
+
+def get_temperature():
+    try:
+        temp = subprocess.check_output(["vcgencmd", "measure_temp"]).decode("utf-8")
+        return float(temp.split("=")[1].split("'")[0])
+    except Exception as e:
+        return str(e)
+
+
+@app.get("/status")
+async def read_status():
+    cpu_temp = get_temperature()
+    cpu_usage = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    return {
+        "cpu_temp": cpu_temp,
+        "cpu_usage": cpu_usage,
+        "memory": memory,
+        "disk_usage": disk_usage,
+    }
+
+
